786fd7cdaab8fa74c1cf981ce8b60afb5dc17694
Harshad Sharma <harshad@sharma.io>
2014-09-25 16:08:30 +0530

    Initial Commit

---
+++ treesnake/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  treesnake
+//
+//  Created by Harshad Sharma on 25/09/14.
+//  Copyright (c) 2014 Harshad Sharma. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ treesnake/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  treesnake
+//
+//  Created by Harshad Sharma on 25/09/14.
+//  Copyright (c) 2014 Harshad Sharma. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
