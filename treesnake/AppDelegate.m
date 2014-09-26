63da7a944d39bda2c0611198a58924a3460223b7
Harshad Sharma <harshad@sharma.io>
2014-09-26 19:23:10 +0530

    Stub for system menu app. Python integration dylib added. Not functional yet.

--- treesnake/AppDelegate.m
+++ treesnake/AppDelegate.m
@@ -7,6 +7,13 @@
 //
 
 #import "AppDelegate.h"
+#import "Python.h"
+
+
+NSStatusItem *statusItem;
+NSMenu       *statusMenu;
+NSStatusBar  *statusBar;
+
 
 @interface AppDelegate ()
 
@@ -17,10 +24,36 @@
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
     // Insert code here to initialize your application
+    
+    statusBar = [NSStatusBar systemStatusBar];
+    statusItem = [statusBar statusItemWithLength:NSVariableStatusItemLength];
+    statusItem.title = @"treesnake";
+    statusItem.highlightMode = YES;
+    
+    // Build on-click menu
+    statusMenu = [[NSMenu alloc] initWithTitle:@""];
+    [statusMenu setAutoenablesItems:NO];
+    
+    [statusMenu addItemWithTitle:@"One" action:nil keyEquivalent:@""];
+    [statusMenu addItemWithTitle:@"Two" action:nil keyEquivalent:@""];
+    [statusMenu addItemWithTitle:@"Three" action:nil keyEquivalent:@""];
+    
+    [statusMenu addItem:[NSMenuItem separatorItem]];
+    
+    // Quit menu option
+    NSMenuItem *tItem = nil;
+    tItem = [statusMenu addItemWithTitle:@"Quit" action:@selector(terminate:) keyEquivalent:@"q"];
+    [tItem setKeyEquivalentModifierMask:NSCommandKeyMask];
+    
+    // Attach the menu
+    [statusItem setMenu:statusMenu];
 }
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
     // Insert code here to tear down your application
+}
+
+- (void) awakeFromNib {
 }
 
 @end
