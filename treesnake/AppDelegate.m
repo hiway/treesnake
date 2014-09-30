0846aaa50da9a80e1eaff6192db3d5d7aead6cc0
Harshad Sharma <harshad@sharma.io>
2014-10-01 00:11:45 +0530

    A nice python habitat now lurks under ~/.treesnake! Able to get strings from Python functions.

--- treesnake/AppDelegate.m
+++ treesnake/AppDelegate.m
@@ -19,6 +19,7 @@ NSStatusItem *statusItem;
 NSMenu       *statusMenu;
 NSStatusBar  *statusBar;
 
+PyObject     *treesnakeLib = NULL;
 
 @interface AppDelegate ()
 
@@ -43,7 +44,7 @@ NSStatusBar  *statusBar;
     //[statusMenu addItemWithTitle:@"Two" action:nil keyEquivalent:@""];
     //[statusMenu addItemWithTitle:@"Three" action:nil keyEquivalent:@""];
     [statusMenu addItemWithTitle:@"Reload" action:@selector(reloadPython:) keyEquivalent:@""];
-
+    [statusMenu addItemWithTitle:@"Test" action:@selector(testPython:) keyEquivalent:@""];
     
     //[statusMenu addItem:[NSMenuItem separatorItem]];
     
@@ -65,7 +66,62 @@ NSStatusBar  *statusBar;
 
 -(void)reloadPython:(id) sender
 {
-    NSLog(@"You asked to reload Python");
+    NSLog(@"Load/Reload Python!");
+    Py_Initialize();
+    const char *pypath = [[[NSBundle mainBundle] resourcePath] UTF8String];
+
+    // import sys
+    PyObject *sys = PyImport_Import(PyString_FromString("sys"));
+
+    // sys.path.append(resourcePath)
+    PyObject *sys_path_append = PyObject_GetAttrString(PyObject_GetAttrString(sys, "path"), "append");
+    PyObject *resourcePath = PyTuple_New(1);
+    PyTuple_SetItem(resourcePath, 0, PyString_FromString(pypath));
+    PyObject_CallObject(sys_path_append, resourcePath);
+
+    // sys.path.append(~/.treesnake/)
+    NSString *homeDir = NSHomeDirectory();
+    NSString *treesnakeDir = [homeDir stringByAppendingString:@"/.treesnake"];
+    const char *pypath1 = [treesnakeDir cStringUsingEncoding:[NSString defaultCStringEncoding]];;
+    PyObject *resourcePath1 = PyTuple_New(1);
+    PyTuple_SetItem(resourcePath1, 0, PyString_FromString(pypath1));
+    PyObject_CallObject(sys_path_append, resourcePath1);
+    
+    // import treesnake   # this is in ~/.treesnake/treesnake.py
+    treesnakeLib = PyImport_Import(PyString_FromString("treesnake"));
+    if (treesnakeLib) {
+        NSLog(@"treesnake module loaded");
+    } else {
+        NSLog(@"treesnake module could not be loaded");
+    }
+}
+
+-(void)testPython:(id) sender
+{
+    NSString *output = call_function(@"refresh_rate");
+    if (output) {
+        NSString *text = output;
+        statusItem.title = text;
+    } else {
+        statusItem.title = @"NULL";
+    }
+}
+
+NSString *call_function(NSString *functionName)
+{
+    NSLog(@"Call function.");
+    if (treesnakeLib) {
+        PyObject *my_func = PyObject_GetAttrString(treesnakeLib, [functionName cStringUsingEncoding:[NSString defaultCStringEncoding]]);
+        if (my_func && PyCallable_Check(my_func)){
+            PyObject *result = PyObject_CallObject(my_func, NULL);
+            return [NSString stringWithFormat:@"%s", PyString_AsString(result)];
+        }
+        else {
+            return @"Function not found or not callable.";
+        }
+    } else {
+        return @"treesnake module not found or not loaded";
+    }
 }
 
 @end
